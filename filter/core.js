42d7f4a653b7240338bc1fa941f4a033f92e3b60
Arlo Shefte-Jacobs <arlo.sj3@gmail.com>
2017-08-29 10:00:37 -0600

    filter

--- filter/core.js
+++ filter/core.js
@@ -1,16 +1,29 @@
-function onlyEven (array) {
-
+function onlyEven(array) {
+  var evens = array.filter(function(num) {
+    return num % 2 === 0;
+  });
+  return evens
 };
 
-function onlyOneWord (array) {
+function onlyOneWord(array) {
   // your code here
+  console.log(array)
+  var ones = array.filter(function(phrase){
+    return !phrase.includes(' ')
+  })
+  return ones
 };
 
-function positiveRowsOnly (array) {
+function positiveRowsOnly(array) {
   // your code here
+return array.filter(function(arr){
+  return arr.every(function(num){
+    return num >= 0
+  })
+})
 };
 
-function allSameVowels (array) {
+function allSameVowels(array) {
   // your code here
 };
 
